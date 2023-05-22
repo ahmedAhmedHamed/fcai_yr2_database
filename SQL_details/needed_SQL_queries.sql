4c1d58c9a6f9e51fcf5f3856612c6060f26bccf3
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-05-22 21:05:20 +0300

    changed sql queries

--- SQL_details/needed_SQL_queries.sql
+++ SQL_details/needed_SQL_queries.sql
@@ -19,11 +19,11 @@ CREATE TABLE aircraft (
 
 -- Creating the flight table
 CREATE TABLE flights (
-    flight_id INT PRIMARY KEY,
+    flight_id INT PRIMARY KEY AUTO_INCREMENT,
     flight_date DATE NOT NULL,
     sourcee VARCHAR(10) NOT NULL,
     destination VARCHAR(50) NOT NULL,
-    aircraft_id INT NOT NULL AUTO_INCREMENT,
+    aircraft_id INT NOT NULL,
     FOREIGN KEY (aircraft_id) REFERENCES aircraft(aircraft_id)
 );
 
@@ -44,42 +44,27 @@ INSERT INTO users (user_id, username, password, user_type)
 VALUES (1, 'ahmed', 'ahmed333', 'admin');
 
 -- Adding a new aircraft
-INSERT INTO aircraft (aircraft_id, aircraft_name, total_seats)
-VALUES (1, 'myaircraft', 100);
+INSERT INTO aircraft ( aircraft_name, total_seats)
+VALUES ('myaircraft', 100);
 
 -- Adding a new flight
 INSERT INTO flights (flight_id, flight_date, sourcee, destination, aircraft_id)
 VALUES (1, '2023-05-09', 'cairo', 'aswan', 1);
 
 -- Updating an aircraft's details
-UPDATE aircraft
-SET total_seats = 300
-WHERE aircraft_id = 1;
+UPDATE aircraft SET total_seats = 300 WHERE aircraft_id = 1;
 
 -- Updating a flight's details
-UPDATE flights
-SET destination = 'usa'
-WHERE flight_id = 1;
+UPDATE flights SET destination = 'usa' WHERE flight_id = 1;
 
 -- Showing a list of available flights that satisfy certain criteria
-SELECT f.flight_id, f.flight_date, f.sourcee, f.destination, a.aircraft_name, a.total_seats
-FROM flights f
-INNER JOIN aircraft a
-ON f.aircraft_id = a.aircraft_id
-WHERE f.flight_date = '2023-06-01'
-AND f.sourcee = 'cairo'
-AND f.destination = 'usa'
-AND a.total_seats >= 2;
+SELECT f.flight_id, f.flight_date, f.sourcee, f.destination, a.aircraft_name, a.total_seats FROM flights f INNER JOIN aircraft a ON f.aircraft_id = a.aircraft_id WHERE f.flight_date = '2023-06-01' AND f.sourcee = 'cairo' AND f.destination = 'usa' AND a.total_seats >= 2;
 
 -- Booking a flight
-INSERT INTO booking (booking_id, user_id, flight_id, class_type, seats_booked)
-VALUES (1, 1, 1, 'business', 2);
+INSERT INTO booking (booking_id, user_id, flight_id, class_type, seats_booked) VALUES (1, 1, 1, 'business', 2);
 
 -- Cancelling a booking
-DELETE FROM booking
-WHERE booking_id = 1;
+DELETE FROM booking WHERE booking_id = 1;
 
 -- Changing flight class
-UPDATE booking
-SET class_type = 'first class'
-WHERE booking_id = 1;
+UPDATE booking SET class_type = 'first class' WHERE booking_id = 1;
