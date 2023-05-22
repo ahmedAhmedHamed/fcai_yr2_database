43ffce487636813460c77d19e7418b369d5d014d
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2023-05-22 05:07:57 +0300

    groundwork for aircraft manager

---
+++ sqlconn/src/AircraftManager.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+
+public class AircraftManager {
+    Connection conn;
+    User current_user;
+
+    AircraftManager(Connection newConn) {
+        newConn = conn;
+    }
+
+
+}
